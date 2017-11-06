f2363a6bd47e44341fd7079c2a7fa883fbdc6b28
Jan Edrozo <jedrozo@gmail.com>
2017-11-06 13:20:43 -0800

    Movie model relations test

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,25 +1,39 @@
 require "test_helper"
 
 describe Customer do
-  it "a customer requires a name" do
-    start_count = Customer.count
+  let(:customer) { customers(:bill) }
 
-    customer = Customer.new
-    customer.save
-    customer.valid?.must_equal false
-    customer.errors.messages.must_include :name
+  describe "validations" do
+    it "a customer requires a name" do
+      start_count = Customer.count
 
-    Customer.count.must_equal start_count
+      invalid_customer = Customer.new
+      invalid_customer.save
+      invalid_customer.valid?.must_equal false
+      invalid_customer.errors.messages.must_include :name
+
+      Customer.count.must_equal start_count
+    end
+
+    it "a customer is created with a name" do
+      start_count = Customer.count
+
+      customer1 = Customer.new(name: "Professor X")
+      customer1.save
+      customer1.valid?.must_equal true
+
+      Customer.count.must_equal start_count + 1
+    end
   end
 
-  it "a customer is created with a name" do
-    start_count = Customer.count
-
-    customer1 = Customer.new(name: "Professor X")
-    customer1.save
-    customer1.valid?.must_equal true
-
-    Customer.count.must_equal start_count + 1
+  describe "relations" do
+    it "has many rentals" do
+      customer.must_respond_to :rentals
+      customer.must_be_kind_of Customer
+      customer.rentals.each do |item|
+        item.must_be_kind_of Rental
+      end
+    end
   end
 
 end
