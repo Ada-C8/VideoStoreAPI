a83fcf8060c379505819890e848c400191ba1cad
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-11-06 15:45:12 -0800

    Add tests for index

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,17 +1,21 @@
 class CustomersController < ApplicationController
   def index
     customers = Customer.all
+    # render(
+    #   json: customers.as_json(only: [:name, :address, :registered_at, :city, :state, :postal_code, :phone, :account_credit]), status: :ok
+    # )
     render(
-      json: customers.as_json(only: [:name, :address, :registered_at, :city, :state, :postal_code, :phone, :account_credit]), status: :ok
+      json: customers.as_json(except: [:created_at, :updated_at]),
+      status: :ok
     )
   end
 
-  def show
-  end
-
-  def create
-  end
-
-  def update
-  end
+  # def show
+  # end
+  #
+  # def create
+  # end
+  #
+  # def update
+  # end
 end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -11,5 +11,36 @@ describe CustomersController do
       get customers_path
       response.header['Content-Type'].must_include 'json'
     end
+
+    it "returns an array" do
+      get customers_path
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all of the customers" do
+      get customers_path
+      body = JSON.parse(response.body)
+      body.length.must_equal Customer.count
+    end
+
+    it "returns the customers with exactly the required fields" do
+      get customers_path
+      body = JSON.parse(response.body)
+      body.each do |customer|
+        customer.keys.sort.must_equal ["account_credit", "address", "city", "id", "name", "phone", "postal_code", "registered_at", "state"]
+      end
+    end
+
+    it "returns an empty array if there are no customers" do
+      Customer.destroy_all
+      get customers_path
+      must_respond_with :success
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+      body.must_be :empty?
+    end
+
+
   end
 end
