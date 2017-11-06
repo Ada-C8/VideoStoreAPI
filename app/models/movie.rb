369b589eeb8b9722bdddc446572bd5fde9d1a6af
Shaunna <skwiens@gmail.com>
2017-11-06 14:57:37 -0800

    validations for movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,4 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :release_date, presence: true
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -3,8 +3,23 @@ require "test_helper"
 describe Movie do
   let(:movie) { Movie.new }
 
-  it "Can be created" do
-    movie.save
-    movie.must_be_instance_of Movie
-  end
+  describe "VALIDATIONS" do
+    it "Can be created" do
+      movie.title = "I'll be stronger tomorrow"
+      movie.release_date= "Mon, 06 Nov 2017 14:32:26 -0800"
+      movie.valid?.must_equal true
+      proc {movie.save}.must_change("Movie.count", 1)
+    end
+
+    it "cannot be created without title, release_date" do
+      movie.valid?.must_equal false
+      movie.errors.must_include :title
+      movie.errors.must_include :release_date
+    end
+
+    it "has an id" do
+
+    end
+
+  end #VALIDATIONS
 end
