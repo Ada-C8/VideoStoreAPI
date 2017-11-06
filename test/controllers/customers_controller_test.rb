8db428bfe5cf7b424ae876149a318054cae1b4a2
Maria McGrew <mcgmar1@outlook.com>
2017-11-06 13:39:45 -0800

    returning movie fields and array passing

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,9 +1,9 @@
 require "test_helper"
 
 describe CustomersController do
-  it "should get index" do
-    get customers_index_url
-    value(response).must_be :success?
-  end
+  # it "should get index" do
+  #   get customers_index_url
+  #   value(response).must_be :success?
+  # end
 
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -17,6 +17,29 @@ describe MoviesController do
       get movies_path
       response.header['Content-Type'].must_include 'json'
     end
+
+    it "returns an Array" do
+      get movies_path
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all of the movies" do
+      get movies_path
+
+      body = JSON.parse(response.body)
+      body.length.must_equal Movie.count
+    end
+
+    it "returns movies with exactly the required fields" do
+      keys = %w(inventory overview release_date title)
+      get movies_path
+      body = JSON.parse(response.body)
+      body.each do |movie|
+        movie.keys.sort.must_equal keys
+      end
+    end
   end
 
   # it "should get show" do
