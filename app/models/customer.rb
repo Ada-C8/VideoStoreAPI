f0ede71871915be0a9ece5bcfe8e441e70978803
kee nam <nam.keeyeon@gmail.com>
2017-11-06 15:36:19 -0800

    Edited text for customer.rb model and routes.rb

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
   has_many :rentals
 
-  validates :name, presence: { message: "Please enter a username"}
+  validates :name, presence: { message: "Please enter a name"}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :customers, only: :index
   resources :movies, only: [:index, :show, :create]
+
+  #make zomg test path
 end
