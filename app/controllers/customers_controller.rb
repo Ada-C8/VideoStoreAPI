a887d1ecc7ba6d6a4bde0524c68af1dc9c48f749
Tanja Stroble <plusdeltah@gmail.com>
2017-11-06 11:42:18 -0800

    adds index/customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,7 @@
 class CustomersController < ApplicationController
+  def index
+    customers = Customer.all
+    render json: customers.as_json(only: [:name, :registered_at, :address, :city, :state, :postal_code, :phone, :account_credit]),
+    status: :ok
+  end
 end
