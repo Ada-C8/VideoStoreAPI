07fadc1dff685d49d9bc2775296605841d1a722e
Gale Harrington <gale.harrington19@gmail.com>
2017-11-06 15:43:41 -0800

    Passing movie show tests

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,17 @@
 class MoviesController < ApplicationController
+  def index
+    movies = Movie.all
+
+    render json: movies.as_json(only: [:id, :title, :release_date]), status: :ok
+  end
+
+  def show
+    movie = Movie.find_by(id: params[:id])
+
+    if movie
+      render json: movie.as_json(only: [:title, :overview, :release_date, :inventory]), status: :ok
+    else
+      render json: { errors: {id: "#{params[:id]} not found"} }, status: :not_found
+    end
+  end
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,61 @@
 require "test_helper"
 
 describe MoviesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+    it "is a real route" do
+      get movies_path
+      must_respond_with :success
+    end
+
+    it "returns json" do
+      get movies_path
+
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "returns an array of hashes" do
+      get movies_path
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+      body.each do |element|
+        element.must_be_kind_of Hash
+      end
+    end
+
+    it "returns all of the movies" do
+      get movies_path
+
+      body = JSON.parse(response.body)
+      body.length.must_equal Movie.count
+    end
+
+    it "returns movies with exactly the required fields" do
+      keys = %w(id release_date title)
+      get movies_path
+      body = JSON.parse(response.body)
+      body.each do |movie|
+        movie.keys.sort.must_equal keys
+      end
+    end
+
+    it "returns an empty array if there are no movies" do
+      Movie.destroy_all
+      get movies_path
+      body = JSON.parse(response.body)
+      body.must_equal []
+    end
+  end
+
+  describe "show" do
+    it "can get a movie" do
+      get movie_path(movies(:magic).id)
+      must_respond_with :success
+    end
+    it "returns an error for invalid id" do
+      movies(:magic).destroy
+      get movie_path(movies(:magic).id)
+      must_respond_with :not_found
+  end
+  end
 end
