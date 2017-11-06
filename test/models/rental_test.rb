120d6067eebe27b4ea1937edc5c40c0c3f3f9f85
Jan Edrozo <jedrozo@gmail.com>
2017-11-06 13:29:10 -0800

    Rental model relations test

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,5 +1,25 @@
 require "test_helper"
 
 describe Rental do
+  let(:rental) { rentals(:rental1)}
+  let(:movie1) { movies(:movie1)}
+  let(:bill) { customers(:bill)}
 
+
+  describe 'relationships' do
+    it "has a customer" do
+      rental.must_respond_to :customer
+      rental.must_be_kind_of Rental
+      rental.customer.must_be_kind_of Customer
+      rental.customer.must_equal bill
+    end
+
+    it "has an movie" do
+      rental.must_respond_to :movie
+      rental.must_be_kind_of Rental
+      rental.movie.must_be_kind_of Movie
+      rental.movie.must_equal movie1
+    end
+
+  end
 end
