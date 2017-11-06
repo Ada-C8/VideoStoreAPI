0e6f843546916861bc589793f53b9cdc414faf30
kee nam <nam.keeyeon@gmail.com>
2017-11-06 15:26:19 -0800

    Added validations and relations to customer Model and added tests

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,9 +1,28 @@
 require "test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
+  let(:customer) { Customer.new() }
 
-  it "must be valid" do
-    value(customer).must_be :valid?
+  describe 'validations' do
+    it "must be valid with a name" do
+      customer.name = "Bruce Wayne"
+      is_valid = customer.valid?
+      is_valid.must_equal true
+    end
+
+    it "will not be valid without a name" do
+      is_valid = customer.valid?
+      is_valid.must_equal false
+    end
+  end
+
+  describe 'relations' do
+    it 'has an association with Rentals' do
+      Customer.reflect_on_association(:rental)
+    end
+
+    it 'responds to Rentals' do
+      customer.must_respond_to :rentals
+    end
   end
 end
