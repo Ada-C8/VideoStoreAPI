8d8d5a923562142c944f4a2559d24309b946bedf
Isaac Del Rosario <cecdelr@gmail.com>
2017-11-06 13:28:19 -0800

    Created test for if movie index returns json

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,7 @@
 class MoviesController < ApplicationController
   def index
+    movies = Movie.all
+    render json: movies.as_json(only: [:title, :overview, :release_date, :inventory]), status: :ok
   end
 
   def show
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -12,6 +12,11 @@ describe MoviesController do
 
       get movies_path
     end
+
+    it "returns a json" do
+      get movies_path
+      response.header['Content-Type'].must_include 'json'
+    end
   end
 
   # it "should get show" do
