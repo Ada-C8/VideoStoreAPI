ad0b16b78616d8b43bcef59336f083b8fb1dbf8e
Shaunna <skwiens@gmail.com>
2017-11-06 13:29:55 -0800

    refactor cust index test

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -2,18 +2,13 @@ require "test_helper"
 
 describe CustomersController do
   describe "index" do
-    it "is a working route" do
+
+    it "returns successfully returns json as an Array" do
       get customers_path
       must_respond_with :success
-    end
 
-    it "returns json" do
-      get customers_path
       response.header['Content-Type'].must_include 'json'
-    end
 
-    it 'returns an array' do
-      get customers_path
       body = JSON.parse(response.body)
       body.must_be_instance_of Array
     end
