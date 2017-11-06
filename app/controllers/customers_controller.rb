ba5b39668a1eaf8ec4c1e98c09ab70a0158186cc
Amy Cash <amymcash@gmail.com>
2017-11-06 15:48:45 -0800

    Test and Add Show method for customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -7,4 +7,16 @@ class CustomersController < ApplicationController
       status: :ok
     )
   end
+
+  def show
+    customer = Customer.find_by(id: params[:id])
+    if customer
+      render(
+        json: customer.as_json(only: [:name, :registered_at, :address, :city, :state, :postal_code, :phone, :account_credit]),
+        status: :ok
+      )
+    else
+      render json: {nothing: true}, status: :not_found
+    end
+  end
 end
