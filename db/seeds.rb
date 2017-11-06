0e10415acacf09c2f751623915c58d28f4f31aeb
Julia <julia.meier@gmail.com>
2017-11-06 14:33:53 -0800

    created movies model and seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,6 @@ JSON.parse(File.read('db/seeds/customers.json')).each do |customer|
   Customer.create!(customer)
 end
 
-#TODO - come back to this
-# JSON.parse(File.read('db/seeds/movies.json')).each do |movie|
-#   Movie.create!(movie)
-# end
+JSON.parse(File.read('db/seeds/movies.json')).each do |movie|
+  Movie.create!(movie)
+end
