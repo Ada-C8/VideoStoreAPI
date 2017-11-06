49b51cb5927c41db4ddd29ec392338da8250f67b
Jan Edrozo <jedrozo@gmail.com>
2017-11-06 13:09:42 -0800

    Movie model test validation

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,27 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { Movie.new }
 
-  it "must be valid" do
-    value(movie).must_be :valid?
+  it "a movie must have a title" do
+    start_count = Movie.count
+
+    movie1 = Movie.new
+    movie1.save
+    movie1.valid?.must_equal false
+    movie1.errors.messages.must_include :title
+
+    Movie.count.must_equal start_count
   end
+
+  it "a movie must have a title" do
+    start_count = Movie.count
+
+    movie = Movie.new(title: "Sleeping B")
+    movie.save
+    movie.valid?.must_equal true
+
+    Movie.count.must_equal start_count + 1
+  end
+
+
 end
