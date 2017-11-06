e21b78f5ce1ccaa6c1c7c7ab0cf32cae5f1126d7
Isaac Del Rosario <cecdelr@gmail.com>
2017-11-06 15:33:02 -0800

    Created index tests for Customer and added logic to Customer controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,6 @@
 class CustomersController < ApplicationController
   def index
+    customers = Customer.all
+    render json: customers.as_json(only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :account_credit]), status: :ok
   end
 end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,9 +1,44 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "should get index" do
-  #   get customers_index_url
-  #   value(response).must_be :success?
-  # end
+  describe "index" do
+    it "is a real working route" do
+      get customers_path
+      must_respond_with :success
+    end
 
+    it "returns an empty array if there are no Customers" do
+      Customer.destroy_all
+
+      get customers_path
+    end
+
+    it "returns a json" do
+      get customers_path
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "returns an Array" do
+      get customers_path
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all of the Customers" do
+      get customers_path
+
+      body = JSON.parse(response.body)
+      body.length.must_equal Customer.count
+    end
+
+    it "returns Customers with exactly the required fields" do
+      keys = %w(account_credit address city id name postal_code registered_at state)
+      get customers_path
+      body = JSON.parse(response.body)
+      body.each do | customer |
+        customer.keys.sort.must_equal keys
+      end
+    end
+  end
 end
