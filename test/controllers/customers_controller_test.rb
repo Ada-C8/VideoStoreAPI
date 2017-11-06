813b04eeab86bf690cbbd67292bf09eb396e9bdf
Tanja Stroble <plusdeltah@gmail.com>
2017-11-06 12:48:31 -0800

    adds controller test for customer/index

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -13,6 +13,32 @@ describe 'index' do
     response.header['Content-Type'].must_include 'json'
   end
 
+  it 'returns an array' do
+    get customers_url
+    body = JSON.parse(response.body)
+    body.must_be_kind_of Array
+  end
+
+  it 'returns an array even if no data' do
+    Customer.destroy_all
+
+    get customers_url
+    body = JSON.parse(response.body)
+    body.must_be_kind_of Array
+    body.count.must_equal 0
+    body.must_equal []
+
+  end
+
+  it "returns customers with exactly the required fields" do
+      keys = %w(name registered_at address city state postal_code phone account_credit)
+      get customers_url
+      body = JSON.parse(response.body)
+      body.each do |pet|
+        pet.keys.must_equal keys
+      end
+    end
+
 end
 
 end
