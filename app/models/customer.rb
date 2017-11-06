f7e0fa0d479b3d47ae4782d3fc70e83cf73ca3e9
Jessica Owens <vertige@gmail.com>
2017-11-06 14:35:50 -0800

    Included registered at in the validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,11 +2,6 @@ class Customer < ApplicationRecord
   validates :name, presence: true
   validates :postal_code, presence: true
   validates :phone, presence: true
+  validates :registered_at, presence: true
 
-  def registered_at # part of validation testing
-    return self.created_at
-  end
-
-  #==========================#
-  
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -5,7 +5,8 @@ describe Customer do
   let(:popo) {Customer.new(
     name: "Popo",
     postal_code: "V5C 6P2",
-    phone: "50650"
+    phone: "50650",
+    registered_at: "Mon, 06 Nov 2017 14:32:26 -0800"
     )
   }
 
@@ -14,6 +15,7 @@ describe Customer do
       customer.name = "Name"
       customer.postal_code = "V5C 6P2"
       customer.phone = "50560"
+      customer.registered_at = "Mon, 06 Nov 2017 14:32:26 -0800"
       customer.must_be_instance_of Customer
     end
 
@@ -27,14 +29,14 @@ describe Customer do
       customer.postal_code = "V5C 6P2"
       customer.valid?.must_equal false
       customer.phone = "50650"
+      customer.registered_at = "Mon, 06 Nov 2017 14:32:26 -0800"
       customer.valid?.must_equal true
     end
 
-    it "Will be created with an id and a registered_at" do
+    it "Will be created with an id" do
       popo.save
       her_id = Customer.find_by(name: "Popo").id
       popo.id.must_equal her_id
-      popo.registered_at.must_equal popo.created_at
     end
   end #VALIDATIONS
 end
