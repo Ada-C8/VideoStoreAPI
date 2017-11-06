15a3716bf73af17d40e6937d6b2ae57bf13b0aa5
Kimberley Zell <kimberley.zell@gmail.com>
2017-11-06 15:16:46 -0800

    added index def to movies and an appropriate route

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,5 +6,12 @@ class MoviesController < ApplicationController
       message: "IT WORKS!"
     }.to_json
   end
-  
+
+  def index
+    movies = Movie.all
+    render(
+      :json => movies.as_json(only: [:id, :title, :overview, :release_date, :inventory]), status: :ok
+    )
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/zomg', to: 'movies#zomg'
+
+  resources :movies, only: [:index]
 end
