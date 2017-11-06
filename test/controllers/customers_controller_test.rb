dbadf5c9d4b1b1503411de8f4fe0be4da80a23da
nkiru onwuneme <generalhrh@gmail.com>
2017-11-06 15:25:05 -0800

    All positive tests passing

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -27,7 +27,14 @@ describe CustomersController do
       body.length.must_equal Customer.count
     end
 
+    it "returns all customers with exactly the required fields" do
+      keys = %w(name registered_at address city state postal_code phone account_credit)
 
-
+      get customers_path
+      body = JSON.parse(response.body)
+      body.each do |customer|
+        customer.keys.must_equal keys
+      end
+    end
   end
 end
