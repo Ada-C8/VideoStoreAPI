72a9b2c1e848194d51e3f35ccf19299587b23bbe
Tanisha <tanishahampden@gmail.com>
2017-11-06 15:21:33 -0800

    add customer#index method

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,6 @@
 class CustomersController < ApplicationController
+  def index
+    customers = Customer.all
+    render json: customers.as_json(only: [:id, :account_credit, :address, :city, :name, :phone, :postal_code, :registered_at, :state])
+  end
 end
