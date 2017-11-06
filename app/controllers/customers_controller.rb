984f0aece48fd6ac9d9156e03b57f3b6b485769b
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-11-06 15:47:57 -0800

    Add show method

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,17 +1,26 @@
 class CustomersController < ApplicationController
   def index
     customers = Customer.all
-    # render(
-    #   json: customers.as_json(only: [:name, :address, :registered_at, :city, :state, :postal_code, :phone, :account_credit]), status: :ok
-    # )
+
     render(
       json: customers.as_json(except: [:created_at, :updated_at]),
       status: :ok
     )
   end
 
-  # def show
-  # end
+  def show
+    customer = Customer.find_by(id: params[:id])
+    if customer
+      render(
+        json: customer.as_json(except: [:created_at, :updated_at]),
+        status: :ok
+      )
+    else
+      render(
+        json: {nothing: true}, status: :not_found
+      )
+    end
+  end
   #
   # def create
   # end
