811d5bf81c03b55731b3757be0b7338f0d83884c
Jan Edrozo <jedrozo@gmail.com>
2017-11-06 13:41:28 -0800

    Added controllers for movies & customers (with customer index method updated)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'customers/index'
+
+  get 'movies/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :customers, only: [:index]
   resources :movies, only: [:index, :show, :create]
