9a0cef3efc8c1783b8af0ccf2d2f4b980be9c1df
nkiru onwuneme <generalhrh@gmail.com>
2017-11-06 15:05:58 -0800

    Passing test for customer index

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,10 @@
 class CustomersController < ApplicationController
+  def index
+    customers = Customer.all
+
+    render(
+      json: customers.as_json(only: [:name, :registered_at, :address, :city, :state, :postal_code, :account_credit]),
+      status: :ok
+    )
+  end
 end
