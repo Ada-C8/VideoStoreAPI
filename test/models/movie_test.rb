7aaeb07bbf665cdf971e16014ffea6a5b37163c8
Laura Robertson <mslaura.mae@gmail.com>
2017-11-06 14:38:04 -0800

    Added model test for movies

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -2,8 +2,17 @@ require "test_helper"
 
 describe Movie do
   let(:movie) { Movie.new }
+  let(:jaws) { movies(:jaws) }
 
   it "must be valid" do
-    value(movie).must_be :valid?
+    jaws.valid?.must_equal true
+    jaws.title = nil
+    jaws.valid?.must_equal false
+  end
+
+  it "must have a title" do
+    movie.valid?.must_equal false
+    movie.title = "A movie"
+    movie.valid?.must_equal true
   end
 end
