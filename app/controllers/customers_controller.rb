a23f981cda5cced5431da0b1040630205da370b9
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-11-06 13:14:48 -0800

    Add customer controller tests, begin index action

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,10 @@
 class CustomersController < ApplicationController
 
   def index
-    
+    customers = Customer.all
+
+    render(
+      json: customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone])
+    )
   end
 end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "#index" do
+    it "returns json" do
+      get customers_path
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "must return an empty arry if no customers exist" do
+      Customer.destroy_all
+      get customer_path
+      must_respond_with :success
+      body = JSON.parse(response.body)
+      body.must_equal []
+    end
+
+    it "will return an array of all customers if they exist" do
+      get customer_path
+      must_respond_with :success
+      body = JSON.parse(response.body)
+      body.must_be_instance_of Array
+      body.length.must_equal Customer.count
+    end
+  end
 end
