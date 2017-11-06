388786c6004e1d6dc325ff5ca916e651c725d331
Mariana <lmarianarp@gmail.com>
2017-11-06 15:10:39 -0800

    index test for model and movies

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,53 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+      it "is a real working route" do
+        get customers_url
+        must_respond_with :success
+      end
+
+      it "returns json" do
+        get customers_url
+        response.header['Content-Type'].must_include 'json'
+      end
+
+      it "returns an Array" do
+        get customers_url
+
+        body = JSON.parse(response.body)
+        body.must_be_kind_of Array
+      end
+
+      it "returns all of the customers" do
+        get customers_url
+
+        body = JSON.parse(response.body)
+        body.length.must_equal Customer.count
+      end
+
+      it "returns customers with exactly the required fields" do
+        keys = %w(id movies_checked_out_count name phone postal_code registered_at)
+        get customers_url
+        body = JSON.parse(response.body)
+        body.each do |customer|
+          customer.keys.sort.must_equal keys
+        end
+      end
+
+      it "returns an empty array if there are no customers" do
+        # Arrange
+        Customer.destroy_all
+
+        # Act
+        get customers_path
+
+        # Assert
+        must_respond_with :success
+        body = JSON.parse(response.body)
+        body.must_be_kind_of Array
+        body.must_be :empty?
+      end
+    end
+
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,55 @@
 require "test_helper"
 
 describe MoviesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+      # These tests are a little verbose - yours do not need to be
+      # this explicit.
+      it "is a real working route" do
+        get movies_url
+        must_respond_with :success
+      end
+
+      it "returns json" do
+        get movies_url
+        response.header['Content-Type'].must_include 'json'
+      end
+
+      it "returns an Array" do
+        get movies_url
+
+        body = JSON.parse(response.body)
+        body.must_be_kind_of Array
+      end
+
+      it "returns all of the movies" do
+        get movies_url
+
+        body = JSON.parse(response.body)
+        body.length.must_equal Movie.count
+      end
+
+      it "returns movies with exactly the required fields" do
+        keys = %w(id release_date title)
+        get movies_url
+        body = JSON.parse(response.body)
+        body.each do |movie|
+          movie.keys.sort.must_equal keys
+        end
+      end
+
+      it "returns an empty array if there are no movies" do
+        # Arrange
+        Movie.destroy_all
+
+        # Act
+        get movies_path
+
+        # Assert
+        must_respond_with :success
+        body = JSON.parse(response.body)
+        body.must_be_kind_of Array
+        body.must_be :empty?
+      end
+    end
+
 end
