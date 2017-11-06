e8833648760f36e825cd4ada5f7194af0c3cd482
Kimberley Zell <kimberley.zell@gmail.com>
2017-11-06 14:31:08 -0800

    got seed data loaded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+MOVIE_FILE = Rails.root.join('db', 'seeds', 'movies.json')
+
+movies = JSON.parse(File.read(MOVIE_FILE))
+#
+# PRODUCT_FILE = Rails.root.join('db', 'seed_data', 'products.csv')
+# puts "Loading raw product data from #{PRODUCT_FILE}"
+
+movies.each do |movie|
+  Movie.create!(movie)
+end
+
+
+CUSTOMER_FILE = Rails.root.join('db', 'seeds', 'customers.json')
+
+customers = JSON.parse(File.read(CUSTOMER_FILE))
+
+customers.each do |customer|
+  Customer.create!(customer)
+end
