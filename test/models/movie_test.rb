80be712beadaa30ba8a574e6bf0415b84d12be3c
enigmagnetic <lauren.cardella@gmail.com>
2017-11-06 13:06:31 -0800

    IS/LC: Write model validations tests.

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,24 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { Movie.new }
+  let(:dune) { movies(:dune) }
+  let(:gremlins) { movies(:gremlins) }
+  describe "validations" do
+    it "must have a title" do
+      dune.title = nil
+      dune.valid?.must_equal false
+    end
 
-  it "must be valid" do
-    value(movie).must_be :valid?
+    it "must have an inventory that is greater than 0" do
+      gremlins.inventory = nil
+      gremlins.valid?.must_equal false
+      gremlins.inventory = 0
+      gremlins.valid?.must_equal false
+    end
+
+    it "must have a release date" do
+      dune.release_date = nil
+      dune.valid?.must_equal false
+    end
   end
 end
