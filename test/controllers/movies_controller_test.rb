bb3f0639fd7071b98812bfb647883c798703481b
Stef <seedsofthesesame@gmail.com>
2017-11-06 14:03:59 -0800

    Movies controller tests created.

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,93 @@
 require "test_helper"
 
 describe MoviesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+    it "is a real working route" do
+      get movies_url
+      must_respond_with :success
+    end
+
+    it "returns json" do
+      get movies_url
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "returns an Array" do
+      get movies_url
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all of the movies" do
+      get movies_url
+
+      body = JSON.parse(response.body)
+      body.length.must_equal Movie.count
+    end
+
+    it "returns movies with exactly the required fields" do
+      keys = %w(title overview release_date inventory)
+      get movies_url
+      body = JSON.parse(response.body)
+      body.each do |movie|
+        movie.keys.sort.must_equal keys
+      end
+    end
+
+    it "returns an empty array if there are no movies" do
+      Movie.destroy_all
+      get movies_url
+      must_respond_with :success
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+      body.must_be :empty?
+    end
+  end
+
+  describe "show" do
+    it "can get a movie" do
+      get movie_path(movies(:two).id)
+      must_respond_with :success
+    end
+  end
+
+  describe "create" do
+    let(:movie_data) {
+      {
+        title: "Jaws",
+        overview: "A great white shark terrorizes townspeople.",
+        release_date: "1975-06-19",
+        inventory: 6
+      }
+    }
+
+    it "Creates a new movie" do
+      assert_difference "Movie.count", 1 do
+        post movies_url, params: { movie: movie_data }
+        assert_response :success
+      end
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+      body.must_include "id"
+
+      # Check that the ID matches
+      Movie.find(body["id"]).title.must_equal movie_data[:title]
+    end
+
+    it "Returns an error for an invalid movie" do
+      bad_data = movie_data.clone()
+      bad_data.delete(:title)
+      assert_no_difference "Movie.count" do
+        post movies_url, params: { movie: bad_data }
+        assert_response :bad_request
+      end
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+      body.must_include "errors"
+      body["errors"].must_include "title"
+    end
+  end
 end
