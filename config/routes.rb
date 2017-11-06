43e6f5468e756b84151b037ec2abe1195e477cc1
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-11-06 14:06:45 -0800

    Creates Customers controller with index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'customers/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :movies, only: [:index, :show, :create]
