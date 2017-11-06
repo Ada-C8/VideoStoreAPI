779d64da9f47da7abf8d02864f4610d59e698dca
Jan Edrozo <jedrozo@gmail.com>
2017-11-06 15:16:06 -0800

    MoviesController #index testing and method update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,21 @@
 class MoviesController < ApplicationController
   def index
+    movies = Movie.all
+
+    render(
+      json: movies.as_json(only: [:id, :title, :release_date]),
+      status: :ok
+    )
   end
+
+  def show
+  end
+
+  def create
+  end
+
+  private
+    def movie_params
+      params.require(:movie).permit(:id, :title, :release_date, :overview, :inventory, :available_inventory)
+    end
 end
