281d19bcc8329dbdf50f988af44664dfaf374bc0
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-11-06 14:22:09 -0800

    Customer index controller passes all tests

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,8 @@
 class MoviesController < ApplicationController
 
   def index
+    movies = Movie.all
+    render json: movies.as_json(only: [:title, :overview, :release_date, :inventory]), status: :ok
   end
 
   def show
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,14 +1,52 @@
 require "test_helper"
 
 describe MoviesController do
-  it "can get the zomg path" do
-    get zomg_path
-    must_respond_with :success
-    body = JSON.parse(response.body)
-    body["message"].must_equal "it works!"
+
+  describe "test-test" do
+    it "can get the zomg path" do
+      get zomg_path
+      must_respond_with :success
+      body = JSON.parse(response.body)
+      body["message"].must_equal "it works!"
+    end
+    it "returns json" do
+      get zomg_path
+      response.header['Content-Type'].must_include 'json'
+    end
   end
-  it "returns json" do
-    get zomg_path
-    response.header['Content-Type'].must_include 'json'
+
+  describe "#index" do
+    before do
+      get movies_path
+    end
+
+    it "is a real route" do
+      must_respond_with :success
+    end
+
+    it "returns an array" do
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+    it "returns all the movies" do
+      body = JSON.parse(response.body)
+      body.length.must_equal Movie.count
+    end
+    it "returns movies with exactly the required fields" do
+      keys = %w(inventory overview release_date title)
+      body = JSON.parse(response.body)
+      body.each do |movie|
+        movie.keys.sort.must_equal keys
+      end
+    end
+    it "returns an empty array is there are no movies" do
+      Movie.destroy_all
+      get movies_path
+      must_respond_with :success
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+      body.must_be :empty?
+    end
   end
+
 end
