7d991582a4a784776114a4a174dff557caf2f413
Isaac Del Rosario <cecdelr@gmail.com>
2017-11-06 14:55:02 -0800

    added id to attributes shared in JSON, makes index tests in Postman pass

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,19 +1,40 @@
 class MoviesController < ApplicationController
   def index
     movies = Movie.all
-    render json: movies.as_json(only: [:title, :overview, :release_date, :inventory]), status: :ok
+    render json: movies.as_json(only: [:id, :title, :overview, :release_date, :inventory]), status: :ok
   end
 
   def show
     movie = Movie.find_by(id: params[:id])
 
     if movie
-      render json: movie.as_json(only: [:title, :overview, :release_date, :inventory]), status: :ok
+      render json: movie.as_json(only: [:id, :title, :overview, :release_date, :inventory]), status: :ok
     else
       render json: { ok: false}, status: :not_found
     end
   end
 
   def create
+    movie = Movie.create(movie_params)
+    if movie.valid?
+      render(
+        json: movie.as_json(only: [:id, :title, :overview, :release_date, :inventory]),
+        status: :created
+      )
+    else
+      render(
+        json:{
+          errors: movie.errors.messages
+        },
+        status: :bad_request
+      )
+    end
+
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:id, :title, :overview, :release_date, :inventory)
   end
 end
