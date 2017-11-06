ffd2b2b406d69e072adfecc7afbacf351b56e62a
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-11-06 14:25:46 -0800

    Refactor index action on customers controller to a method action for setting movies_checked_out_count

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,13 @@
 class Customer < ApplicationRecord
 
+  def self.index_customers
+    customers = Customer.all
+    customers_array = []
+    customers.each do |customer|
+      customer_hash = customer.as_json(only: [:id, :name, :registered_at, :postal_code, :phone]).merge('movies_checked_out_count' => 0)
+      customers_array << customer_hash
+    end
+    return customers_array
+  end
+
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -8,4 +8,28 @@ describe Customer do
     customer.save.must_equal true
   end
 
+  describe "self.index_customers" do
+    before do
+      @customers = Customer.index_customers
+    end
+    it "returns an array of hashes" do
+      @customers.must_be_instance_of Array
+      @customers.length.must_equal Customer.count
+      @customers.each do |customer|
+        customer.must_be_instance_of Hash
+      end
+    end
+
+    it "contains appropriate customer information" do
+      @customers.each do |customer|
+        customer.keys.must_include "id"
+        customer.keys.must_include "name"
+        customer.keys.must_include "registered_at"
+        customer.keys.must_include "postal_code"
+        customer.keys.must_include "phone"
+        customer.keys.must_include "movies_checked_out_count"
+      end
+    end
+  end
+
 end
