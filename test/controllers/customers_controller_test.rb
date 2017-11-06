89f2bd0736d1d1fec33c7103b8044944e2f5f4e3
Shaunna <skwiens@gmail.com>
2017-11-06 13:07:31 -0800

    customer index route and action

---
+++ test/controllers/customers_controller_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+describe CustomersController do
+  describe "index" do
+    it "is a working route" do
+      get customers_path
+      must_respond_with :success
+    end
+
+  end
+end
