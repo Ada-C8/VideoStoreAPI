95d4c13cd3ed4d335dd8fb4e71c88ca4c78880bf
Jan Edrozo <jedrozo@gmail.com>
2017-11-06 13:33:26 -0800

    Added rails routes (for customers and movies)

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -2,26 +2,27 @@ require "test_helper"
 
 describe Movie do
 
-  it "a movie requires a title" do
-    start_count = Movie.count
+  describe "validations" do
+    it "a movie requires a title" do
+      start_count = Movie.count
 
-    movie1 = Movie.new
-    movie1.save
-    movie1.valid?.must_equal false
-    movie1.errors.messages.must_include :title
+      movie1 = Movie.new
+      movie1.save
+      movie1.valid?.must_equal false
+      movie1.errors.messages.must_include :title
 
-    Movie.count.must_equal start_count
+      Movie.count.must_equal start_count
+    end
+
+    it "a movie is created with a title" do
+      start_count = Movie.count
+
+      movie = Movie.new(title: "Sleeping B")
+      movie.save
+      movie.valid?.must_equal true
+
+      Movie.count.must_equal start_count + 1
+    end
   end
-
-  it "a movie is created with a title" do
-    start_count = Movie.count
-
-    movie = Movie.new(title: "Sleeping B")
-    movie.save
-    movie.valid?.must_equal true
-
-    Movie.count.must_equal start_count + 1
-  end
-
 
 end
