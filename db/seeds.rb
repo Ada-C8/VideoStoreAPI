506fcb115a34a6699c881a4d9e667af232664c77
Amy Cash <amymcash@gmail.com>
2017-11-06 14:19:57 -0800

    Add seeds to models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+movies = JSON.parse(File.read('db/seeds/movies.json'))
+
+movies.each do |movie|
+  Movie.create!(movie)
+end
+
+
+
+json = JSON.parse(File.read('db/seeds/customers.json'))
+
+json.each do |c|
+  Customer.create!(c)
+end
