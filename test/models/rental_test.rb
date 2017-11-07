e81f5c121a622960f3fb0c42068d7d2218f3bd6a
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-11-07 11:15:49 -0800

    Basic rentals relationship model tests passing with addition of two rental objects in yaml file

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -5,6 +5,8 @@ describe Rental do
   #rentals YAML
   let(:rental_one) { rentals :rental_one }
   let(:rental_two) { rentals :rental_two }
+  let(:rental_three) { rentals :rental_three }
+  let(:rental_four) { rentals :rental_four }
 
   #customer YAML
   let(:customer_one) { customers :customer_one}
@@ -27,12 +29,14 @@ describe Rental do
 
     it "allows one movie to have many customers" do
       movie_one.customers.count.must_be :>, 1
-      # movie_one.customers.must_include customer_one
-      # movie_one.customers.must_include customer_two
+      movie_one.customers.must_include customer_one
+      movie_one.customers.must_include customer_two
     end
 
     it "allows one customer to have many movies" do
-
+      customer_one.movies.count.must_be :>, 1
+      customer_one.movies.must_include movie_one
+      customer_one.movies.must_include movie_two
     end
 
   end
