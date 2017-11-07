549a8b3bffa469a84ab9a86fb7dd4658f9ec776f
enigmagnetic <lauren.cardella@gmail.com>
2017-11-07 13:41:54 -0800

    Add rentals routes for checkin, checkout, and index.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 
   get 'customers/', to: 'customers#index', as: 'customers'
 
+  post 'rentals/checkout', to: 'rentals#create', as: 'checkout'
+  put 'rentals/:id/checkin', to: 'rentals#update', as: 'checkin'
+  get 'rentals/overdues', to: 'rentals#index', as: 'rentals'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
