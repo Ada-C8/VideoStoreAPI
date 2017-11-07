0563508029e82451fb34947503a1fdb713f03eee
Stef <seedsofthesesame@gmail.com>
2017-11-07 10:29:43 -0800

    Updated Customers controller and model, added serializer.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,3 +1,9 @@
 class CustomersController < ApplicationController
+  def index
+    customers = Customer.all
 
+    render(
+      json: customers, status: :ok
+    )
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,7 @@
 class Customer < ApplicationRecord
   has_many :rentals
+
+  def movies_checked_out_count
+    return 0
+  end
 end
