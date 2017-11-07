18af8757a59e87b615fa0b8e9aa717061606f158
Angela Wilson <wilsonange@gmail.com>
2017-11-06 16:12:41 -0800

    write movie controller index action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,12 @@
 class MoviesController < ApplicationController
+
+  def index
+    movies = Movie.all
+
+    #ensureing all the movie records are formated in json with the specific categories
+    render(
+      json: movies.as_json(only: [:inventory, :overview, :release_date, :title]),
+      status: :ok
+    )
+  end
 end
