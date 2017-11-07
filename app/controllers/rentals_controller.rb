b5dc8f3c1698146a163c4e1f82f1ac0517d8d9f4
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-11-07 11:56:44 -0800

    implement checkin for rentals

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -8,6 +8,17 @@ class RentalsController < ApplicationController
     end
   end
 
+  def checkin
+    rental = Rental.find_by(movie_id: params[:movie_id], customer_id: params[:customer_id])
+    if rental && rental.status == 'checked_out'
+      rental.status = 'returned'
+      rental.save!
+      render json: {id: rental.status}
+    else
+      render json: {nothing: true}, status: :bad_request
+    end
+  end
+
   private
   def rental_params
     params.permit(:movie_id, :customer_id, :due_date)
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -5,6 +5,8 @@ describe RentalsController do
   let(:valid_customer_id) { Customer.first.id }
   let(:invalid_customer_id) { Customer.last.id + 1 }
   let(:params) {{ movie_id: valid_movie_id, customer_id: valid_customer_id }}
+  let(:rental) { rentals(:one) }
+  let(:checkin_params) { { movie_id: rental.movie_id, customer_id: rental.customer_id } }
 
   describe "checkout" do
     it "creates a new rental" do
@@ -30,9 +32,30 @@ describe RentalsController do
       }.wont_change 'Rental.count'
     end
   end
-  describe "checkin" do
 
+  describe "checkin" do
+    it "changes an existing rental's status to 'returned' " do
+      post checkin_path, params: checkin_params
+
+      must_respond_with :success
+      rental_id = rental.id
+
+      Rental.find(rental_id).status.must_equal 'returned'
+    end
+    it "returns a bad request if rental does not exist" do
+      checkin_params['customer_id'] = invalid_customer_id
+      post checkin_path, params: checkin_params
+
+      must_respond_with :bad_request
+    end
+    it "returns a bad request if rental is already returned" do
+      post checkin_path, params: checkin_params
+      post checkin_path, params: checkin_params
+
+      must_respond_with :bad_request
+    end
   end
+
   describe "overdue" do
 
   end
