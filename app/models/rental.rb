e5487c55d9e58355cce340603b5de8c5ab3f49e5
Tanja Stroble <plusdeltah@gmail.com>
2017-11-06 16:37:44 -0800

    adds formatting methods for date

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,2 +1,15 @@
+require 'date'
+
 class Rental < ApplicationRecord
+  before_create :format_checkout, :format_due_date
+
+  private
+  def format_checkout
+    checkout_date = Date.today.strftime('%Y-%m-%d')
+  end
+
+  def format_due_date
+    due_date = (Date.today + 10).strftime('%Y-%m-%d')
+  end
+
 end
