3092add71f6f55d23655277da903540ddc130b29
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-11-07 11:06:07 -0800

    initial rental model relationship test passing with change in YAML rental test file

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -6,10 +6,37 @@ describe Rental do
   let(:rental_one) { rentals :rental_one }
   let(:rental_two) { rentals :rental_two }
 
-  #
+  #customer YAML
+  let(:customer_one) { customers :customer_one}
+  let(:customer_two) { customers :customer_two}
 
+  #movie YAML
+  let(:movie_one) { movies :movie_one}
+  let(:movie_two) { movies :movie_two}
 
-  it "must be valid" do
-    value(rental).must_be :valid?
+  describe "relations" do
+    it "has a customer" do
+      rental_one.must_respond_to :customer
+      rental_one.customer.must_be_kind_of Customer
+    end
+
+    it "has a movie" do
+      rental_one.must_respond_to :movie
+      rental_one.movie.must_be_kind_of Movie
+    end
+
+    it "allows one movie to have many customers" do
+      movie_one.customers.count.must_be :>, 1
+      movie_one.customers.must_include customer_one
+      movie_one.customers.must_include customer_two
+    end
+
+    it "allows one customer to have many movies" do
+
+    end
+
   end
+  # it "must be valid" do
+  #   value(rental).must_be :valid?
+  # end
 end
