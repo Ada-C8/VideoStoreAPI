3e4c536e486081b289cb57f6a0e9416a279967df
Amy Lee <amy.yj.lee@gmail.com>
2017-11-07 12:50:19 -0800

    added routes and placeholder methods for Rental controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,12 @@ Rails.application.routes.draw do
 
   resources :customers, only: :index
 
+  post '/rentals/check-out', to: 'rentals#check_out', as: 'checkout' #checkout_path
+
+  post '/rentals/check-in', to: 'rentals#check_in', as:
+  'checkin' #checkin_path
+
+  get '/rentals/overdue', to: 'rentals#overdue', as: 'overdue' #overdue_path
+
   get '/zomg', to: 'movies#check', as: 'zomg' #zomg_path
 end
