7df0e976cc8908e17b1fcb1488c4c22f91abcec9
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-11-06 19:02:02 -0800

    Add update method

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -37,13 +37,31 @@ class CustomersController < ApplicationController
       )
     end
   end
-  #
-  # def update
-  # end
+
+  def update
+    customer = Customer.find_by(id: params[:id])
+    if customer
+      customer.update_attributes(customer_params)
+      if customer.save
+        render(
+          json: customer.as_json(except: [:created_at, :updated_at]),
+          status: :ok
+        )
+      else
+        render(
+          json: {errors: customer.errors.messages}, status: :bad_request
+        )
+      end
+    else
+      render(
+        json: {nothing: true}, status: :not_found
+      )
+    end
+  end
 
   private
 
   def customer_params
-    params.require(:customer).permit(:name, :registered_at, :address, :city, :state, :postal_code, :phone, :account_credit)
+    params.permit(:name, :registered_at, :address, :city, :state, :postal_code, :phone, :account_credit)
   end
 end
