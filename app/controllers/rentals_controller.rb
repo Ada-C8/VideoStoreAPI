9dfcf9320618bcbb8875d9068202221de284d3eb
Sara Frandsen <sara.frandsen@gmail.com>
2017-11-07 09:59:40 -0800

    begin first rental controller test

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,6 +1,10 @@
+require 'date'
+
 class RentalsController < ApplicationController
   def check_out
-
+    rental = customer_id(movie_id)
+    movie_id.available_inventory - 1
+    rental.checkout_date = Date.new(Date.today)
   end
 
   def check_in
@@ -8,7 +12,7 @@ class RentalsController < ApplicationController
   end
 
   def overdue
-    
+
   end
 
 end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -3,10 +3,20 @@ require "test_helper"
 describe RentalsController do
   describe 'check_out' do
     it "successfully removes a movie from available_inventory" do
+      post rentals_check_out_path(movies(:one).id)
 
+      before_count = available_inventory.count
+      must_respond_with :success
+
+      response.header('Content-Type').must_include 'json'
+      body = JSON.parse(respnse.body)
+      body.must_be_kind_of Array
+
+      available_inventory.must_equal before_count - 1
     end
 
     it "has a customer_id, movie_id, and due_date" do
+
     end
 
     it "doesn't check out a movie if avialble_inventory is 0" do
@@ -40,6 +50,6 @@ describe RentalsController do
     it "isn't errors if it isn't truly overdue" do
     end
 
-    
+
   end
 end
