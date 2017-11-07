d3d947126faeeb9e654c3040d457547b432f1d68
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-11-06 16:16:49 -0800

    did something but not really

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,9 @@ require "rails/test_help"
 require "minitest/rails"
 require "minitest/reporters"  # for Colorized output
 
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:
 # require "minitest/rails/capybara"
