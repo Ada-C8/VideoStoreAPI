7083a38973c86412ba06e5df29838e6456a52f6f
Jan Edrozo <jedrozo@gmail.com>
2017-11-07 11:34:23 -0800

    Rental Controller #checkout negative testing complete

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -29,6 +29,14 @@ describe RentalsController do
       }
     }
 
+    let(:bad_rental_data) {
+      {
+        movie_id: -2,
+        customer_id: -2,
+        due_date: "2017-12-24"
+      }
+    }
+
     it "can successfully checkout" do
       assert_difference "Rental.count", 1 do
         post rental_checkout_path, params: rental_data
@@ -42,8 +50,17 @@ describe RentalsController do
       body["checkout_date"].must_equal (Date.today).strftime('%Y-%m-%d')
 
       Rental.find(body["id"]).customer_id.must_equal rental_data[:customer_id]
+    end
 
+    it "returns an error for an invalid rental" do
+      post rental_checkout_path, params: bad_rental_data
+      assert_response :bad_request
 
+      body =  JSON.parse(response.body)
+      body.must_be_kind_of Hash
+      body.must_include "errors"
+      body["errors"].must_include "customer"
+      body["errors"].must_include "movie"
     end
 
 
