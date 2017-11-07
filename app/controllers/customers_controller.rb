ebc66be67ce272fd89bd7657785ff9fe4aa7b439
Jan Edrozo <jedrozo@gmail.com>
2017-11-07 09:41:17 -0800

    Removed .json code (and added attribute readers in CustomerSerializer)

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,7 +3,7 @@ class CustomersController < ApplicationController
     customers = Customer.all
 
     render(
-      json: customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count]),
+      json: customers,
       status: :ok
     )
   end
