6624110bb5f875ec3109a05c690b58c34e6ca83f
enigmagnetic <lauren.cardella@gmail.com>
2017-11-07 12:54:19 -0800

    Generate rentals controller. Add set_due_date method to model.

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,6 +1,15 @@
 class Rental < ApplicationRecord
+  RENTAL_PERIOD = 5
   belongs_to :movie
   belongs_to :customer
 
+  before_create :set_due_date
+
   validates :due_date, presence: true
+
+  private
+
+    def set_due_date
+      self.due_date = Date.today + RENTAL_PERIOD
+    end
 end
