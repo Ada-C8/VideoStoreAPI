deef71972e8ad588d4946e7ef1d58e13cc693fa6
Maria McGrew <mcgmar1@outlook.com>
2017-11-07 13:43:33 -0800

    added movies_checked_out method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,8 @@ class Customer < ApplicationRecord
 
   validates :name, :registered_at, :address, :city, :state, :postal_code, :phone, presence: true
   validates :account_credit, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  def movies_checked_out_count
+    return self.rentals.count
+  end
 end
