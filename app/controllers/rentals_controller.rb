aa92e6e7480f8866a1d94af473119a674bb06fa1
Tanja Stroble <plusdeltah@gmail.com>
2017-11-07 12:50:28 -0800

    adds checkin and checkout pseudocode and tests

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,16 +1,26 @@
 class RentalsController < ApplicationController
 
-  def check_out
+  def check_out(movie_id, customer_id)
+    movie = Movie.find_by_id(movie_id)
+    customer = Customer.find_by_id(customer_id)
+    if movie && movie.available_inventory > 0
+      movie.available_inventory -= 1
+    else
+      error
+    end
+    if customer
+      customer.movies_checked_out_count += 1
+    else
+      error
+    end
+      ++ render json status 201 created
+    end
+
+    def check_in
+    end
+    
+    def overdue
+    end
+
+
   end
-
-  def check_in
-  end
-
-  def overdue
-  end
-
-  def create
-  end
-
-
-end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -5,6 +5,31 @@ describe RentalsController do
       post checkout_path
       must_respond_with :success
     end
+
+    it "decrements number of movies available in inventory" do
+
+    end
+
+    it "increments number of movies customer has rented" do
+
+    end
+
+    it "doesn't succeed for customer that doesn't exit " do
+
+    end
+
+    it "doesn't succeed for movie that doesn't exit " do
+
+    end
+
+    it "doesn't succeed if zero inventory " do
+
+    end
+
+    it "succeeds if sufficient inventory " do
+
+    end
+
   end
 
   describe "check_in" do
@@ -12,12 +37,41 @@ describe RentalsController do
       post checkin_path
       must_respond_with :success
     end
+
+    it "increments number of movies available in inventory" do
+
+    end
+
+    it "decrements number of movies customer has rented" do
+
+    end
+
+    it "doesn't succeed for customer that doesn't exit " do
+
+    end
+
+    it "doesn't succeed for movie that doesn't exit " do
+
+    end
+
   end
 
   describe "overdue" do
     it "succeeds" do
       get overdue_path
       must_respond_with :success
+    end
+
+    it "returns list  of all customers with overdue movies" do
+
+    end
+
+    it "returns empty array if no customers with overdue movies" do
+
+    end
+
+    it "succeeds wether there are any overdue customers" do
+
     end
   end
 
