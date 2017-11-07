dc24bea946adce786aadb1ec64d015ce3f6aaa3f
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-11-07 10:30:59 -0800

    Rental model validation testing

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -16,4 +16,33 @@ describe Rental do
     )
     rental.save.must_equal true
   end
+
+  describe "won't create invalid rental" do
+    before do
+      @invalid_rental = Rental.new()
+      @invalid_rental.save
+    end
+
+    it "requires a valid customer_id" do
+      @invalid_rental.errors.messages.must_include :customer_id
+      @invalid_rental.errors.messages[:customer_id].must_include "Customer ID is required."
+      @invalid_rental.errors.messages[:customer_id].must_include "Customer ID must be an integer."
+    end
+
+    it "requires a valid movie_id" do
+      @invalid_rental.errors.messages.must_include :movie_id
+      @invalid_rental.errors.messages[:movie_id].must_include "Movie ID is required."
+      @invalid_rental.errors.messages[:movie_id].must_include "Movie ID must be an integer."
+    end
+
+    it "requires a due_date" do
+      @invalid_rental.errors.messages.must_include :due_date
+      @invalid_rental.errors.messages[:due_date].must_include "Due date is required."
+    end
+
+    it "requires a checkout_date" do
+      @invalid_rental.errors.messages.must_include :checkout_date
+      @invalid_rental.errors.messages[:checkout_date].must_include "Checkout date is required."
+    end
+  end
 end
