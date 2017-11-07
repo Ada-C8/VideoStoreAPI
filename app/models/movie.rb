dcefa3b30c090441b11344aa20ba44cfa5119682
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-11-06 16:55:41 -0800

    starts validating title uniqueness within same year scope. Not finished

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,8 +3,16 @@ class Movie < ApplicationRecord
   has_many :customers, through: :rentals
 
   validates :title, presence: true
+  # validates :title, uniqueness: true, if: :same_year?
+  #
+  # def same_year?
+  #   same_title = Movie.where(title: )
+  #
+  # end
+
+
   validates :overview, presence: true
   validates :release_date, presence: true, format: { with: /\d{4}\-\d{2}-\d{2}/}
-  validates :inventory, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :inventory, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
 end
