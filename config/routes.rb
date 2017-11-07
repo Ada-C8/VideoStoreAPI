1f269de7c10f5415ff039a2c98845ed52c076a70
Kimberley Zell <kimberley.zell@gmail.com>
2017-11-07 12:08:09 -0800

    made route for rentals checkout

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/zomg', to: 'movies#zomg'
-
+  get '/rentals/checkout', to: 'rentals#checkout', as: 'checkout'
+  
   resources :movies, only: [:index, :show, :create]
   resources :customers, only: [:index, :show, :create]
+
 end
