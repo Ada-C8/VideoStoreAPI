55f15fcab4369f112812d39118c7a3fc299f45f1
Isaac Del Rosario <cecdelr@gmail.com>
2017-11-07 10:22:58 -0800

    Created validation test for Customer and implemented validation in model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,4 @@
 class Customer < ApplicationRecord
+  validates :name, :registered_at, :address, :city, :state, :postal_code, :phone, presence: true
+  validates :account_credit, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
