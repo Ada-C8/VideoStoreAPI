5c42cb709a1a390f67701ae92609d19ab34015e2
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-11-06 16:47:21 -0800

    Customers controller index passes all tests

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,6 @@
 class CustomersController < ApplicationController
   def index
+    customers = Customer.all
+    render json: customers.as_json(only: [:registered_at, :name, :address, :city, :state, :postal_code, :phone]), status: :ok
   end
 end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,9 +1,38 @@
 require "test_helper"
 
 describe CustomersController do
-  it "should get index" do
-    get customers_path
-    value(response).must_be :success?
+  describe "#index" do
+    before do
+      get customers_path
+    end
+
+    it "is a real route" do
+      must_respond_with :success
+    end
+
+    it "returns an array" do
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+    it "returns all the customers" do
+      body = JSON.parse(response.body)
+      body.length.must_equal Customer.count
+    end
+    it "returns customers with exactly the required fields" do
+      keys = %w(address city name phone postal_code registered_at state)
+      body = JSON.parse(response.body)
+      body.each do |customer|
+        customer.keys.sort.must_equal keys
+      end
+    end
+    it "returns an empty array is there are no customers" do
+      Customer.destroy_all
+      get customers_path
+      must_respond_with :success
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+      body.must_be :empty?
+    end
   end
 
 end
