23a9e31bdc80886842fa075a5ee7f57693176466
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-11-07 10:49:15 -0800

    Add rental YAML data and begin work on rental relationship testing

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -2,9 +2,9 @@ class Rental < ApplicationRecord
   belongs_to :customer
   belongs_to :movie
 
-  validates :customer_id, presence: true, numericality: { only_integer: true }
-  validates :movie_id, presence: true, numericality: { only_integer: true }
-  validates :checkout_date, presence: true
-  validates :due_date, presence: true
+  # validates :customer_id, presence: true, numericality: { only_integer: true }
+  # validates :movie_id, presence: true, numericality: { only_integer: true }
+  # validates :checkout_date, presence: true
+  # validates :due_date, presence: true
 
 end
--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 describe Rental do
-  let(:rental) { Rental.new }
+
+  #rentals YAML
+  let(:rental_one) { rentals :rental_one }
+  let(:rental_two) { rentals :rental_two }
+
+  #
+
 
   it "must be valid" do
     value(rental).must_be :valid?
