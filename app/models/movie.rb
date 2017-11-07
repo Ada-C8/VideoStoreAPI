d5be498abaac4d7651ff9e2aac6e60d25fbba775
Maria McGrew <mcgmar1@outlook.com>
2017-11-07 10:00:40 -0800

    added model validations to movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,4 @@
 class Movie < ApplicationRecord
-  validates :title, :overview, :release_date, :inventory, presence: true
+  validates :title, :overview, :release_date, presence: true
+  validates :inventory, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 } 
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,43 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { Movie.new }
+  let (:movie) {Movie.new}
+  let (:one) {movies(:one)}
+  let (:two) {movies(:two)}
 
-  it "must be valid" do
-    value(movie).must_be :valid?
+  it "must have a title to be vaild" do
+    one.valid?.must_equal true
+    one.title = nil
+    one.valid?.must_equal false
+  end
+
+  it "must have a overview to be vaild" do
+    one.valid?.must_equal true
+    one.overview = nil
+    one.valid?.must_equal false
+  end
+
+  it "must have a release date to be vaild" do
+    one.valid?.must_equal true
+    one.release_date = nil
+    one.valid?.must_equal false
+  end
+
+  it "must have an integer in inventory to be vaild" do
+    one.valid?.must_equal true
+    one.inventory = "zero"
+    one.valid?.must_equal false
+  end
+
+  it "inventory must be greater than or equal to zero to be vaild" do
+    one.valid?.must_equal true
+    one.inventory = -1
+    one.valid?.must_equal false
+  end
+
+  it "must have an integer in inventory to be vaild" do
+    one.valid?.must_equal true
+    one.inventory = nil
+    one.valid?.must_equal false
   end
 end
