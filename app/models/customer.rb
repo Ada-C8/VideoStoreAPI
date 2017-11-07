6ffb8a5a00ace470fd3df55d03feabba2f616699
Lauren <Laurenelee88@gmail.com>
2017-11-07 09:31:21 -0800

    through relationships

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,7 @@
 class Customer < ApplicationRecord
   has_many :rentals
-  
+  has_many :movies, :through => :rentals
+
   validates :name, presence: true
   validates :phone, presence: true
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
   has_many :rentals
-  
+  has_many :cutomsters, :through => :rentals
   validates :title, presence: true
   validates :inventory, presence: true
 
