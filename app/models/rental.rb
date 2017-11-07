4dc7c7861443ae1af3db7ea45b50d046071a8270
Kimberley Zell <kimberley.zell@gmail.com>
2017-11-07 10:38:03 -0800

    set up model validations for rentals

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,10 @@
 class Rental < ApplicationRecord
   belongs_to :customer
   belongs_to :movie
+
+  validates :customer_id, presence: true, numericality: { only_integer: true }
+  validates :movie_id, presence: true, numericality: { only_integer: true }
+  validates :checkout_date, presence: true
+  validates :due_date, presence: true
+
 end
