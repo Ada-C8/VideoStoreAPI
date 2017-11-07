9811e3099a05587c3e549b75aaf88616bfaa58c7
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-11-07 10:46:51 -0800

    Add serializer to customer

---
+++ app/serializers/customer_serializer.rb
@@ -0,0 +1,4 @@
+class CustomerSerializer < ActiveModel::Serializer
+  attributes :id, :name, :registered_at, :address, :city, :state, :postal_code, :phone, :account_credit, :movies_checked_out_count
+
+end
