290219ae3b6f1b452d7cb5c6d8aa9bfe6da237a9
Julia <julia.meier@gmail.com>
2017-11-07 14:38:37 -0800

    added checkin date column to rentals

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107174531) do
+ActiveRecord::Schema.define(version: 20171107222927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20171107174531) do
     t.integer "movie_id", null: false
     t.date "due_date", null: false
     t.date "checkout_date", null: false
+    t.date "checkin_date"
   end
 
   add_foreign_key "rentals", "customers"
