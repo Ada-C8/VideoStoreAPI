46398c21329df28ea8038741d21900b4964b3fa7
Amy Cash <amymcash@gmail.com>
2017-11-07 11:44:15 -0800

    add due date method

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,5 +1,7 @@
 class RentalsController < ApplicationController
   def checkout
+    rental = Rental.new(rental_params)
+    rental.save
   end
 
   def checkin
@@ -7,4 +9,16 @@ class RentalsController < ApplicationController
 
   def overdue
   end
+
+
+private
+
+def rental_params
+  due_date = self.due_date
+  # puts due_date
+  params.require(:rental).permit(:movie_id, :customer_id, :due_date)
+end
+
+
+
 end
