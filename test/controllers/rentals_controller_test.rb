6d0fc25a268943891b050bc3ac254019a3322c42
nkiru onwuneme <generalhrh@gmail.com>
2017-11-07 11:42:50 -0800

    Created rental test files

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 describe RentalsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "checkout" do
+    let(:rental_data) {
+      {
+        movie_id: 1,
+        customer_id: 1,
+        due_date: Date.now + 4.days
+      }
+    }
+
+    it "creates a checkout" do
+      proc {
+        post rentals_checkout_path, params: {rental: rental_data}
+      }.must_change 'Rental.count', 1
+
+      must_respond_with :success
+    end
+  end
 end
