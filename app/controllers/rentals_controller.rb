a8685ceb49740a4e6a57faa0512792dd36e257d0
Tanisha <tanishahampden@gmail.com>
2017-11-07 11:21:24 -0800

    add rentals#checkout functionality and tests

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,15 @@
 class RentalsController < ApplicationController
+  def checkout
+    rental = Rental.new(rental_params)
+    if rental.save
+      render json: {id: rental.id}
+    else
+      render json: {errors: rental.errors.messages}, status: :bad_request
+    end
+  end
+
+  private
+  def rental_params
+    params.permit(:movie_id, :customer_id, :due_date)
+  end
 end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 describe RentalsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  let(:valid_movie_id) { Movie.first.id }
+  let(:valid_customer_id) { Customer.first.id }
+  let(:invalid_customer_id) { Customer.last.id + 1 }
+  let(:params) {{ movie_id: valid_movie_id, customer_id: valid_customer_id }}
+
+  describe "checkout" do
+    it "creates a new rental" do
+      proc {
+        post checkout_path, params: params
+      }.must_change 'Rental.count', 1
+
+      must_respond_with :success
+    end
+    it "returns a json" do
+      post checkout_path, params: params
+      response.header['Content-type'].must_include 'json'
+    end
+    it "returns a hash" do
+      post checkout_path, params: params
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+    end
+    it "returns a bad request if rental cannot be created" do
+      proc {
+        params[:customer_id] = invalid_customer_id
+        post checkout_path, params: params
+      }.wont_change 'Rental.count'
+    end
+  end
+  describe "checkin" do
+
+  end
+  describe "overdue" do
+
+  end
 end
