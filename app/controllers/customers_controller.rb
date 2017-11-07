43f868d65d09fb92798227a0c73cf97f5aa63987
kee nam <nam.keeyeon@gmail.com>
2017-11-06 16:27:54 -0800

    Added movies_checked_out_count column to customers table in DB and passed all customer #index controller tests

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,11 @@
 class CustomersController < ApplicationController
   def index
+    customers = Customer.all
+
+    render(
+      json: customers.as_json(only: [:id, :movies_checked_out_count, :name, :phone, :postal_code, :registered_at]),
+      status: :ok
+    )
   end
 
   def test
