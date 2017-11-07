5ca00c845346c57194f11eb1e8d219848f611b6a
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-11-07 13:40:38 -0800

    Adds a movies_checked_out field to the Customer Serializer

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,3 +1,3 @@
 class CustomerSerializer < ActiveModel::Serializer
-  attributes :registered_at, :name, :address, :city, :state, :postal_code, :phone
+  attributes :registered_at, :name, :address, :city, :state, :postal_code, :phone, :movies_checked_out
 end
