b5d1b60ce4f52eced2447b3cb3c52b30de4a6f4c
Tanja Stroble <plusdeltah@gmail.com>
2017-11-06 16:14:39 -0800

    adds rentals controller and model and sets up rentals routes

---
+++ app/controllers/rentals_controller.rb
@@ -0,0 +1,13 @@
+class RentalsController < ApplicationController
+
+  def check_out
+  end
+
+  def check_in
+  end
+
+  def overdue
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   resources :customers
   resources :movies
 
+  post '/rentals/check_out', to: 'rentals#check_out', as: 'checkout'
+  post '/rentals/check_in', to: 'rentals#check_in', as: 'checkin'
+  get '/rentals/overdue', to: 'rentals#overdue', as: 'overdue'
 end
