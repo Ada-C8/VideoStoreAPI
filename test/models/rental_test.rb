5ee769862177b6a620e1864947e17e8a9ff8bdac
Jan Edrozo <jedrozo@gmail.com>
2017-11-07 15:56:01 -0800

    added extra custom rental model method tests

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -34,10 +34,22 @@ describe Rental do
       Movie.find_by(id: rental.movie_id).available_inventory.must_equal movie_count - 1
     end
 
+    it "increases the customers movies_checked_out_count" do
+      customer_count = Customer.find_by(id: rental.customer_id).movies_checked_out_count
+      rental.remove_movie
+      Customer.find_by(id: rental.customer_id).movies_checked_out_count.must_equal customer_count + 1
+    end
+
     it "does not decrease the available inventory if insufficient inventory to checkout " do
       movie_count = Movie.find_by(id: rental2.movie_id).available_inventory
       rental2.remove_movie
       Movie.find_by(id: rental2.movie_id).available_inventory.must_equal movie_count
+    end
+
+    it "does not increase the customers movies_checked_out_count if insufficient inventory to checkout" do
+      customer_count = Customer.find_by(id: rental2.customer_id).movies_checked_out_count
+      rental2.remove_movie
+      Customer.find_by(id: rental2.customer_id).movies_checked_out_count.must_equal customer_count
     end
 
     it "increases the available inventory when a movie is checked back in" do
@@ -47,10 +59,22 @@ describe Rental do
       Movie.find_by(id: rental.movie_id).available_inventory.must_equal movie_count + 1
     end
 
+    it "decreases the customers movies_checked_out_count" do
+      customer_count = Customer.find_by(id: rental.customer_id).movies_checked_out_count
+      rental.return_movie
+      Customer.find_by(id: rental.customer_id).movies_checked_out_count.must_equal customer_count - 1
+    end
+
     it "does not increase the available inventory if a rental does not have a checkout date" do
       movie_count = Movie.find_by(id: rental2.movie_id).available_inventory
       rental2.return_movie
       Movie.find_by(id: rental2.movie_id).available_inventory.must_equal movie_count
+    end
+
+    it "does not decrease the customers movies_checked_out_count if insufficient inventory to checkout" do
+      customer_count = Customer.find_by(id: rental2.customer_id).movies_checked_out_count
+      rental2.return_movie
+      Customer.find_by(id: rental2.customer_id).movies_checked_out_count.must_equal customer_count
     end
 
   end
