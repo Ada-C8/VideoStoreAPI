4ce2d49a4d41a5b759dd6320d3c6d906b7c34fee
Sara C <srachandler@gmail.com>
2017-11-07 11:28:21 -0800

    movie controller tests all pass

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -51,9 +51,40 @@ describe MoviesController do
  end
 
  describe "create" do
+   let(:movie_data) {
+      {
+        release_date: "Nov",
+        title: "10 Things I hate about you"
+      }
+    }
    it "creates a new movie" do
-     
+     assert_difference "Movie.count", 1 do
+        post movies_path, params: { movie: movie_data }
+        assert_response :success
+      end
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+      body.must_include "id"
+
+
+      Movie.find(body["id"]).title.must_equal movie_data[:title]
    end
+
+   it "Returns an error for an invalid movie" do
+      bad_data = movie_data.clone()
+      bad_data.delete(:title)
+      assert_no_difference "Movie.count" do
+        post movies_path, params: { movie: bad_data }
+        assert_response :bad_request
+      end
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+      body.must_include "errors"
+      body["errors"].must_include "title"
+    end
+
+
  end
 
 end
