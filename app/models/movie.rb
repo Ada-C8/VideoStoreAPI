33e2bb4d608eb062de09939f18af391898d55acf
Tanisha <tanishahampden@gmail.com>
2017-11-08 09:51:30 -0800

    add available_inventory to movies table

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,7 +4,4 @@ class Movie < ApplicationRecord
   validates :title, presence: true
   validates :inventory, presence: true, numericality: true
 
-  def available_inventory
-    inventory
-  end
 end
