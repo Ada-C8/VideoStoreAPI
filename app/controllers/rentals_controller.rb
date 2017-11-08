0bd6cdbef050e62c1a93d6e21eafe68b85c18020
Amy Lee <amy.yj.lee@gmail.com>
2017-11-07 16:54:00 -0800

    added belongs to relationship for Rental

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -12,26 +12,31 @@ class RentalsController < ApplicationController
     week_from_today = Date.today + 7
     rental.due_date = week_from_today.to_s
 
-
     if rental.save
       movie.available_inventory -= 1
       movie.save
       customer.movies_checked_out += 1
       customer.save
-
       # render json: rental.as_json(only: [:movie_id, :customer_id, :pretty_date]), status: :ok
       render json: rental, status: :created
-
-
     else
       render json: { ok: false, errors: rental.errors }.as_json, status: :bad_request
     end
 
-
   end
 
   def check_in
+    movie = Movie.find_by_id(params[:movie_id])
+    customer = Customer.find_by_id(params[:customer_id])
+    customer_checked_out = customer.movies.where(due_date: nil)
 
+
+    #check that a customer has this movie (through rental)
+    if customer.movies.where()
+      #checking_in is the movie to be checked in
+      checking_in = customer.movies.find_by(id: params[:movie_id])
+      if checking_in
+        checking_in.
   end
 
   def overdue
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,6 +1,9 @@
 class Rental < ApplicationRecord
   attr_reader :pretty_date
 
+  belongs_to :movie
+  belongs_to :customer
+
   def self.pretty_date
     d = Date.parse(self.due_date)
     return d.strftime('%a %d %b %Y')
