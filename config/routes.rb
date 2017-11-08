4ca4330c4f84528d9c37a2e1890d96717931289d
Julia <julia.meier@gmail.com>
2017-11-08 10:40:54 -0800

    created overdue route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/movies/:id', to: 'movies#show', as: 'movie'
   post '/movies', to: 'movies#create', as: 'create_movie'
 
+  get '/rentals/overdue', to: 'rentals#overdue', as: 'overdue_rentals'
   post '/rentals/check-out', to: 'rentals#create', as: 'create_rental'
   post '/rentals/check-in', to: 'rentals#update', as: 'update_rental'
 end
