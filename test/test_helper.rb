8b765ad6f836eed59ee949d336ea196e3c09b791
IsabelDePapel <isuchanek@gmail.com>
2017-11-08 11:15:43 -0800

    add filters to simple cov to filter out mailers, jobs, and channels files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,9 @@
 require 'simplecov'
-SimpleCov.start 'rails'
+SimpleCov.start 'rails' do
+  add_filter "/mailers/"
+  add_filter "/jobs/"
+  add_filter "/channels/"
+end
 ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("../../config/environment", __FILE__)
