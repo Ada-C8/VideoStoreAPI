9f4cb34c8f35e2f6f57261fd60f54bdcabd8ccb2
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-11-08 10:13:54 -0800

    add check out count to customers model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,7 +4,11 @@ class Customer < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :postal_code, :account_credit
   validates :account_credit, numericality: true
 
-  def movies_checked_out_count
-    0
+  after_initialize :set_defaults
+
+  private
+
+  def set_defaults
+    self.movies_checked_out_count ||= 0
   end
 end
