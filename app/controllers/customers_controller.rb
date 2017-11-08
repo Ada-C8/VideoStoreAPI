787d8ba203f46175b19b88e1f45d21bca21571d4
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-11-08 11:38:57 -0800

    Remove duplication of index customers method

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,15 +1,10 @@
 class CustomersController < ApplicationController
 
   def index
-    customers = Customer.all
-    customers_array = []
-    customers.each do |customer|
-      customer_hash = customer.as_json(only: [:id, :name, :registered_at, :postal_code, :phone]).merge('movies_checked_out_count' => customer.checkout_count)
-      customers_array << customer_hash
-    end
+    customers = Customer.index_customers
 
     render(
-      json: customers_array.to_json
+      json: customers.to_json
     )
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,7 +5,7 @@ class Customer < ApplicationRecord
     customers = Customer.all
     customers_array = []
     customers.each do |customer|
-      customer_hash = customer.as_json(only: [:id, :name, :registered_at, :postal_code, :phone]).merge('movies_checked_out_count' => 0)
+      customer_hash = customer.as_json(only: [:id, :name, :registered_at, :postal_code, :phone]).merge('movies_checked_out_count' => customer.checkout_count)
       customers_array << customer_hash
     end
     return customers_array
