66808067f4a72f2a1e479e793aaa3a97ce728066
Amy Lee <amy.yj.lee@gmail.com>
2017-11-08 09:51:30 -0800

    added logic to find a Rental given a customer id and movie id

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -27,16 +27,33 @@ class RentalsController < ApplicationController
 
   def check_in
     movie = Movie.find_by_id(params[:movie_id])
+
     customer = Customer.find_by_id(params[:customer_id])
-    customer_checked_out = customer.movies.where(due_date: nil)
+
+    customers_movies = customer.movies.where(id: movie.id)
+
+    if customers_movies.count < 1
+      #return error
+    elsif customers_movies.count == 1
+      Rental.where("movie_id = ? AND customer_id = ?", movie.id, customer.id)
+      #right movie, find the corresponding Rental, check this one in
+    else
+      #get the first corresponding that isn't checked in already
+    end
+
+
+    # customer_checked_out = customer.movies.where(due_date: nil)
+
 
 
     #check that a customer has this movie (through rental)
-    if customer.movies.where()
-      #checking_in is the movie to be checked in
-      checking_in = customer.movies.find_by(id: params[:movie_id])
-      if checking_in
-        checking_in.
+    # if customer.movies.where()
+    #   #checking_in is the movie to be checked in
+    #   checking_in = customer.movies.find_by(id: params[:movie_id])
+    #   if checking_in
+    #     checking_in.
+    #   end
+    # end
   end
 
   def overdue
