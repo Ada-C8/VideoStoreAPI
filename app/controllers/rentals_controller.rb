6a8367d844daae89835f5c64bec0664b4e20d6b4
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-11-08 10:22:16 -0800

    add customer check_out count functionality to checkin and checkout, DRY out rentals controller

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,26 +1,24 @@
 class RentalsController < ApplicationController
   def checkout
-    rental = Rental.new(rental_params)
-    if rental.save
-      movie = rental.movie
-      movie.available_inventory -= 1
-      movie.save
-      render json: {id: rental.id}
+    @rental = Rental.new(rental_params)
+    if @rental.save
+      update_checkout
+      render json: {id: @rental.id}
     else
-      render json: {errors: rental.errors.messages}, status: :bad_request
+      render json: {errors: @rental.errors.messages}, status: :bad_request
     end
   end
 
   def checkin
-    rental = Rental.find_by(movie_id: params[:movie_id], customer_id: params[:customer_id])
-    if rental
-      if rental.status == 'checked_out'
-        rental.status = 'returned'
-        rental.save
-        movie = rental.movie
-        movie.available_inventory += 1
-        movie.save
-        render json: {status: rental.status}
+    @rental = Rental.find_by(movie_id: params[:movie_id], customer_id: params[:customer_id])
+    if @rental
+      if @rental.status == 'checked_out'
+        @rental.status = 'returned'
+        @rental.save
+
+        update_checkin
+
+        render json: {status: @rental.status}
       else
         render json: {:errors => {"rental" => "is already checked in"}}, status: :bad_request
       end
@@ -36,5 +34,21 @@ class RentalsController < ApplicationController
   private
   def rental_params
     params.permit(:movie_id, :customer_id, :due_date)
+  end
+
+  def update_checkout
+    movie, customer = @rental.movie, @rental.customer
+    movie.available_inventory -= 1
+    movie.save
+    customer.movies_checked_out_count += 1
+    customer.save
+  end
+
+  def update_checkin
+    movie, customer = @rental.movie, @rental.customer
+    movie.available_inventory += 1
+    movie.save
+    customer.movies_checked_out_count -= 1
+    customer.save
   end
 end
