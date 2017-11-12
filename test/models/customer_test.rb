0e6bf6a85ad70ed15dfeb67a0c759ffbf9c4e045
Tanja Stroble <plusdeltah@gmail.com>
2017-11-12 12:22:27 -0800

    adds customer model tests

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,5 +1,14 @@
 require "test_helper"
 
 describe Customer do
+  let(:one) { customers(:one) }
 
+  it "must be valid with all fields present" do
+    one.valid?.must_equal true
+  end
+
+  it "is invalid with missing name" do
+    one.name = nil
+    one.valid?.must_equal false
+  end
 end
