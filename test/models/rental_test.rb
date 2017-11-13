665af688409b1ec9d5cb855a87c6010c74047e54
Sara C <srachandler@gmail.com>
2017-11-12 16:06:06 -0800

    merging fixed with rental test and movie controller test

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -2,21 +2,20 @@ require "test_helper"
 
 describe Rental do
   let(:rental_one) { rentals(:rental_one) }
-  let(:rental_new) { Rental.new }
+  let(:rental) { Rental.new }
 
   it "exists" do
     rental_one.valid?.must_equal true
   end
+
   it "must have a due date, customer and movie id" do
-    rental_new.valid?.must_equal false
-    rental_new.due_date = "due date"
-    rental_new.valid?.must_equal false
-    rental_new.customer_id = customers(:customer_one)
-    rental_new.valid?.must_equal false
-    rental_new.movie_id = movies(:movie_one)
-    rental_new.valid?.must_equal false
-    rental_new.save
-    puts rental_new
-    # rental_new.valid?.must_equal true
+    rental.valid?.must_equal false
+    rental.due_date = "due date"
+    rental.valid?.must_equal false
+    rental.customer_id = customers(:customer_one).id
+    rental.valid?.must_equal false
+    rental.movie_id = movies(:movie_one).id
+    rental.valid?.must_equal true
   end
+
 end
